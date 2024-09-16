bb39a55d2b622d58c64bc85589a2f43de4f4c74b
김장호 <rlawkdgh0406@naver.com>
2024-09-16 16:32:56 +0900

    feat: mutation 완료

--- graphQL_learn/server.js
+++ graphQL_learn/server.js
@@ -2,17 +2,23 @@ import { ApolloServer, gql } from "apollo-server"
 
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
   }
   type Tweet {
-    id: ID
-    text: String
-    author: User
+    id: ID!
+    text: String!
+    author: User!
   }
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+  }
+
+  # Mutations란 서버 데이터를 업데이트하도록 서버에 요청을 보내는 동작(POST, DELETE, PUT)
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `
 
